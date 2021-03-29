ef4bda7b4fee21c524a176102647b3ecc6be8b47
Ladawan Paksin <ladawannk.19@gmail.com>
2021-03-29 15:48:32 +0700

    insert

---
+++ playground/1.php
@@ -0,0 +1,41 @@
+<?php
+require_once 'connectdb.php';
+
+    $strSQL = "SELECT  id, username, status FROM user";
+    $result = $myconn->query($strSQL);
+     
+   
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+
+       <table  border="5" width="100%">
+          <tr>
+              <td> ID User</td>
+              <td> User Name</td>
+              <td> Status</td>
+          </tr>
+    <?php
+     while ($row = $result->fetch_array()) {
+        // echo $row["username"] . "<br>";
+        ?>
+
+         <tr>
+             <td> <?php echo $row["id"]?></td>
+             <td> <?php echo $row["username"] ?></td>
+             <td> <?php echo $row["status"]?></td>
+         </tr>
+         <?php
+    }
+    ?>
+        </table>
+    </body>
+
+    </html>
---
+++ playground/index.php
@@ -0,0 +1,52 @@
+<?php
+//$arr = array(1,2,3,4,5,6,7,8,9,10);
+//for($i=0; $i < count($arr); $i++){
+//    if(($arr[$i] % 2) == 0)
+//    echo $arr[$i] . "<br>";
+//}
+
+//$arr = array(1,2,3,4,5,6,7,8,9,10);
+//foreach ($arr as $key => $value) {
+   // if(($key % 2)){
+   //    continue;
+    //}
+   // echo "key = " . $key ."<br>";
+   // echo "Value = " . $value . "<br>";
+
+// $i = 1;
+// while ($i <= 10) {
+//     echo $i ."<br>";
+//     $i++;
+// }
+
+// do{
+//     echo $i . "<br>";
+//     $i++;
+// }while($i <= 10);
+// $a = 5;
+// switch ($a) {
+//     case 5:
+//         echo "a equal 5";
+//        break;
+//     case ($a > 3 && $a < 6):
+//         echo "a gt 5";
+//         break;
+//     case ($a < 5):
+//         echo "a less 5";
+//         break;
+//     default:
+//         echo "default";
+//         break;
+// }
+function calnumber($num1, $num2 = 7)
+{
+    $a = $num1;
+    $b = $num2;
+    $c = $a + $b;
+    $num1 = 6;
+    return $c;
+}
+//// $num1 = 5;
+// $num2 = 6;
+echo calnumber($num1,9);
+echo $num1;
